14df4b79fce7e08a4ebe8ecd1ae6a0eabd46e794
V. A. Canales L <victorcanlima@gmail.com>
2022-05-31 06:30:11 -0500

    Finished function align_datasets

--- align_dataset.m
+++ align_dataset.m
@@ -1,64 +1,93 @@
-function [signals, states, bins_indexes, signals_indexes] = align_dataset(signals_dir, states_dir)
+function [signals, sleep_states, signals_indexes, bins_num] = align_dataset(signals_dirpath, states_dirpath)
 % Align Dataset to get aligned data matrix and bin indexes
 %   Detailed explanation goes here
 
-%signals_dir = "D:\Dev\MATLAB\GenzelLab\data\hpcbel";
-%states_dir = "D:\Dev\MATLAB\GenzelLab\data\sleepstates";
+%signals_dirpath = "D:\Dev\MATLAB\GenzelLab\data\hpcbel";
+%states_dirpath = "D:\Dev\MATLAB\GenzelLab\data\sleepstates";
 
 fn = 600;
-
-ALIGNtot = [26 19 26 10 277 206 247 54 10 11 10 3 16 9 6 6 5 1 0];
-              
+ALIGNtot = [26 19 26 10 277 206 247 54 10 11 10 3 16 9 6 6 5 1 0];     
 remove_at_start = 15 * 60 * fn;
-
 bin_size = 45*60*fn;
 
-files_path_contents = dir(signals_dir);
-are_files = ~cell2mat({files_path_contents.isdir});
-dir_files = {files_path_contents(are_files).name};
-files_order = zeros(1,length(dir_files));
-for i = 1:length(dir_files)
-    file_i = dir_files{i};
+% We get all signal file names and order them (files should be named by a
+% number (Rat ID), this allows to be sure of which row represent wich rat
+signals_dirpath_contents = dir(signals_dirpath);
+are_files = ~cell2mat({signals_dirpath_contents.isdir});
+signals_files = {signals_dirpath_contents(are_files).name};
+ordered_files = zeros(1,length(signals_files));
+for i = 1:length(signals_files)
+    file_i = signals_files{i};
     [~, name, ~] = fileparts(file_i);
-    files_order(i) = str2double(name);
+    ordered_files(i) = str2double(name);
 end
-files_order = sort(files_order);
+ordered_files = sort(ordered_files);
+rats_num = length(ordered_files);
 
-sigs = cell(1, length(files_order));
-states = cell(1, length(files_order));
-for i = 1:length(files_order)
-    name = [num2str(files_order(i)) '.mat'];
-    sig_path = fullfile(signals_dir, name);
-    % Sleep states across the signal
-    sleep_states_path = fullfile(states_dir, name);
-    clusters = importdata(sleep_states_path);
-    sleep_states = zeros(1, length(clusters)*fn*10);
-    for j = 1:length(clusters)
-        state = repmat(clusters(j),1,fn*10);
+% Now array with indices of start and end for every signal is initialized
+signals_indexes = zeros(rats_num, 2);
+
+% We will align all signals and sleep states in cell arrays. Here 15
+% minutes of data are cutted out, signals are trimed to the length of the
+% sleep states and both states and signals are aligned with respect to ALIGNtot
+sigs = cell(1, length(rats_num));
+states = cell(1, length(rats_num));
+for i = 1:rats_num
+    name = [num2str(ordered_files(i)) '.mat'];
+    % We form Sleep State complete vector from 10 second segments
+    states_path = fullfile(states_dirpath, name);
+    state_segments = importdata(states_path);
+    sleep_scoring = zeros(1, length(state_segments)*fn*10);
+    for j = 1:length(state_segments)
+        state = repmat(state_segments(j),1,fn*10);
         pos = fn*10*j-5999;
-        sleep_states(pos:pos+5999) = state;
+        sleep_scoring(pos:pos+5999) = state;
     end
-    sleep_states = sleep_states(remove_at_start:end);
-    states{i} = sleep_states;
-    L = length(sleep_states);
+    % We load signal
+    signal_path = fullfile(signals_dirpath, name);
+    sig = importdata(signal_path);
+    % First 15 minutes of data get cutted out
+    sleep_scoring = sleep_scoring(remove_at_start:end);
+    sig = sig(remove_at_start:end);
+    % Signal is trimmed to the length of built sleep states vector
+    L = length(sleep_scoring);
+    sig = sig(1:L);
+    % Both signal and states are aligned
     align_size = ALIGNtot(i) * 60 * fn;
     alignment = NaN(align_size, 1);
-    sig = importdata(sig_path);
-    cutted_sig = sig(remove_at_start:end);
-    cutted_sig = cutted_sig(1:L);
-    aligned_sig = [alignment; cutted_sig];
+    aligned_states = [alignment; sleep_scoring'];
+    aligned_sig = [alignment; sig];
+    % Processed signal and states are saved in cell array
+    states{i} = aligned_states;
     sigs{i} = aligned_sig;
+    % Indexes of the signal in aligned matrix are first saved here
+    signals_indexes(i,1) = align_size + 1;
+    signals_indexes(i,2) = length(aligned_sig);
 end
 
+% Now we build the aligned matrix from the cell arrays just with a NaN
+% matrix using the signal with maximum length as reference
 max_len = max(cellfun(@length, sigs));
-signals = NaN(length(sigs), max_len);
-for i = 1:length(sigs)
+signals = NaN(rats_num, max_len);
+sleep_states = NaN(rats_num, max_len);
+for i = 1:rats_num
     signals(i, 1:length(sigs{i})) = sigs{i};
+    sleep_states(i, 1:length(states{i})) = states{i};
 end 
 
+% Now the total number of bins (with the bin_size defined duration) is
+% calculated and matrices are trimmed to the number of samples needed for
+% those bins
 bins_num = floor(length(signals)/bin_size);
-
 signals = signals(:, 1:bins_num*bin_size);
+sleep_states = sleep_states(:, 1:bins_num*bin_size);
+% As we just trimmed the matrix, we make sure that any signal top index
+% that was trimmed is updated
+for i = 1:rats_num
+    if signals_indexes(i,2) >= bins_num*bin_size
+        signals_indexes(i,2) = bins_num*bin_size;
+    end
+end
 
 end
 
