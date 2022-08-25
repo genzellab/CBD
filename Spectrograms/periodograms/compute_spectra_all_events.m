f8fb80b07b44220b5af11969810d2a68eff942b8
Pelin Özsezer <pelinozsezer@gmail.com>
2022-08-25 05:40:27 +0300

    Add files via upload

---
+++ Spectrograms/periodograms/compute_spectra_all_events.m
@@ -0,0 +1,190 @@
+%% Pelin Ozsezer
+
+%% PERIODOGRAMS
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Vehicle - All events %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc
+clear all
+close all
+addpath('/Users/pelinozsezer/Documents/Science/Radboud/CBD-Project/fieldtrip');
+addpath('/Users/pelinozsezer/Documents/Science/Radboud/CBD-Project/functions');
+
+load GC_veh.mat
+
+% Combine all events and select 4 sec of time window centered around the peak
+GC_all_events_veh=[GC_ripple_veh.waveforms;GC_sw_veh.waveforms;...
+GC_swr_veh.waveforms;GC_complex_swr_veh.waveforms];
+GC_all_events_veh=cellfun(@(x) extract_range(x,1801,-1200,1200),GC_all_events_veh,'UniformOutput',false);
+
+% Fieldtrip format
+input1=GC_all_events_veh;
+fn=600;
+leng=length(input1);
+ro=1200;
+tm = create_timecell(ro,leng);
+
+channel = [{'PFCshal'},{'HPCpyra'},{'HPCbelo'},{'PFCdeep'}];
+
+label=channel;
+data_all_events.label=label;
+data_all_events.time=tm;
+data_all_events.trial=input1.';
+
+%% Filtering
+% remove line noise
+cfg                = [];
+cfg.channel        = {'all'};
+cfg.dftfilter      = 'yes';
+data_all_events        = ft_preprocessing(cfg,data_all_events);
+
+%% Compute power spectra
+cfg1         = [];
+cfg1.overlap = 0.25;
+cfg1.length        = 4;
+rpt_all_events     = ft_redefinetrial(cfg1, data_all_events);
+
+% periodogram
+cfg2 = [];
+cfg2.output  = 'pow';
+cfg2.channel = 'PFCshal';
+cfg2.method  = 'mtmfft'; % multitaper
+cfg2.taper   = 'hanning';
+cfg2.keeptrials = 'yes';
+
+cfg2.foi     = 0.5:0.25:100; % 1/cfg1.length  = 4;
+freq_all_events = ft_freqanalysis(cfg2, rpt_all_events);
+power_spec=squeeze(freq_all_events.powspctrm);
+
+%% Calculate STerror & mean
+sterror_all_events_veh=std(log(power_spec)) / log(sqrt(length(power_spec)));
+mean_all_events_veh=mean(log(power_spec),1);
+freq=freq_all_events.freq;
+
+clearvars -except sterror_all_events_veh mean_all_events_veh freq
+save mean_sterror_veh.mat
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+%% CBD - All events %%
+%%%%%%%%%%%%%%%%%%%%%%
+
+clc
+clear all
+close all
+addpath('/Users/pelinozsezer/Documents/Science/Radboud/CBD-Project/fieldtrip');
+addpath('/Users/pelinozsezer/Documents/Science/Radboud/CBD-Project/functions');
+
+load GC_cbd.mat
+
+% Combine all events and select 4 sec of time window centered around the peak
+GC_all_events_cbd=[GC_ripple_cbd.waveforms;GC_sw_cbd.waveforms;...
+GC_swr_cbd.waveforms;GC_complex_swr_cbd.waveforms];
+GC_all_events_cbd=cellfun(@(x) extract_range(x,1801,-1200,1200),GC_all_events_cbd,'UniformOutput',false);
+
+% Fieldtrip format
+input1=GC_all_events_cbd;
+fn=600;
+leng=length(input1);
+ro=1200;
+tm = create_timecell(ro,leng);
+
+channel = [{'PFCshal'},{'HPCpyra'},{'HPCbelo'},{'PFCdeep'}];
+
+label=channel;
+data_all_events.label=label;
+data_all_events.time=tm;
+data_all_events.trial=input1.';
+
+%% Filtering
+% remove line noise
+cfg                = [];
+cfg.channel        = {'all'};
+cfg.dftfilter      = 'yes';
+data_all_events        = ft_preprocessing(cfg,data_all_events);
+
+%% Compute power spectra
+cfg1         = [];
+cfg1.overlap = 0.25;
+cfg1.length        = 4;
+rpt_all_events     = ft_redefinetrial(cfg1, data_all_events);
+
+% periodogram
+cfg2 = [];
+cfg2.output  = 'pow';
+cfg2.channel = 'PFCshal';
+cfg2.method  = 'mtmfft'; % multitaper
+cfg2.taper   = 'hanning';
+cfg2.keeptrials = 'yes';
+
+cfg2.foi     = 0.5:0.25:100; % 1/cfg1.length  = 4;
+freq_all_events = ft_freqanalysis(cfg2, rpt_all_events);
+power_spec=squeeze(freq_all_events.powspctrm);
+
+
+%% Calculate STerror & mean
+sterror_all_events_cbd=std(log(power_spec)) / log(sqrt(length(power_spec)));
+mean_all_events_cbd=mean(log(power_spec),1);
+freq=freq_all_events.freq;
+
+clearvars -except sterror_all_events_cbd mean_all_events_cbd freq
+save mean_sterror_cbd.mat
+
+%%%%%%%%%%%%%%
+%% Plotting %%
+%%%%%%%%%%%%%%
+clear
+load('mean_sterror_veh.mat');
+load('mean_sterror_cbd.mat');
+
+figure;
+
+%% Veh
+y = mean_all_events_veh; % mean vector
+x = log10(freq);
+
+curve1 = mean_all_events_veh-sterror_all_events_veh;
+curve2 = mean_all_events_veh+sterror_all_events_veh;
+x2 = [x, fliplr(x)];
+inBetween = [curve1, fliplr(curve2)];
+
+p=fill(x2, inBetween,'g');
+p.FaceAlpha = .6;
+p.FaceColor='#A6A6A6';
+p.EdgeColor='#A6A6A6';
+hold on
+
+plot(x, y, 'k', 'LineWidth', 1.5);
+ylim([-6 10])
+
+xlabel('Frequency (Hz)');
+ylabel('absolute power (uV^2)');
+title('Periodogram - Veh vs CBD')
+
+%% CBD
+y = mean_all_events_cbd; % your mean vector;
+x = log10(freq);
+
+curve1 = mean_all_events_cbd-sterror_all_events_cbd;
+curve2 = mean_all_events_cbd+sterror_all_events_cbd;
+x2 = [x, fliplr(x)];
+inBetween = [curve1, fliplr(curve2)];
+
+p=fill(x2, inBetween,'g');
+p.FaceAlpha = .6;
+p.FaceColor='#53BF33';
+p.EdgeColor='#53BF33';
+hold on
+
+plot(x, y, 'LineWidth', 1.5);
+ylim([-6 10])
+
+legend('SE Veh','Mean Veh','SE CBD','Mean CBD')
+
+saveas(gcf,'periodogram_all_events.jpg')
+saveas(gcf,'periodogram_all_events.pdf')
+saveas(gcf,'periodogram_all_events.fig')
+close all
+
